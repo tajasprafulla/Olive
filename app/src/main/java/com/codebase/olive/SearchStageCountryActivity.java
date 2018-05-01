a49989d64838f0bcb2bd3e6995d4d23c8e99feb4
Tejas.Joshi <tajasprafulla@gmail.com>
2018-05-01 22:55:53 +0530

    Added tab activity

---
+++ app/src/main/java/com/codebase/olive/SearchStageCountryActivity.java
@@ -0,0 +1,13 @@
+package com.codebase.olive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchStageCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_stage_country);
+    }
+}
